4fe110f75546739a53c2fb196668fbb6898cf28e
Oscar Miniet <ominiet96@hotmail.com>
2017-09-06 10:18:46 -0400

    commit

--- listingController.js
+++ listingController.js
@@ -2,13 +2,31 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+    $scope.codeFilt = "";
+    $scope.new = {};
+    $scope.ind = 0;
+    $scope.temp = {"code": "abc", "name": "def"}
+    
+
 
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+
+    $scope.setInd = function(index){
+      $scope.ind = index;
+    }
+    $scope.addListing = function() {
+      $scope.listings.push($scope.new);
+      $scope.new = {};
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+    };
+    $scope.showDetails = function() {
+
+      return $scope.listings[$scope.ind];
+    };
   }
 ]);
