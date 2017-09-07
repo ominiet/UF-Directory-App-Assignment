9c264d2f4dc9f0adeb3782654864d64aab6dcce3
Oscar Miniet <ominiet96@hotmail.com>
2017-09-06 22:19:47 -0400

    should work

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -7,14 +7,45 @@ var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    config = require('./config'),
+    mongo = require('mongodb').MongoClient,
+    assert = require('assert');
 
 /* Connect to your database */
 
+/*
+mongo.connect(config.db.uri, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+
+  
+  modelStuff();
+
+  db.close();
+});
+*/
+
+mongoose.Promise = global.Promise; 
+mongoose.connect(config.db.uri, { useMongoClient: true });
+
+  var data = fs.readFileSync('./listings.json');
+  var listingData = JSON.parse(data.toString());
+
+
+  for (let x of listingData.entries) {
+  const newListing = new Listing(x);
+  newListing.save(function(err) {
+    if (err) throw err;
+      console.log(x.code);
+    });
+  }
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
+
+
  */
+  mongoose.disconnect();
 
 
 /* 
--- queries.js
+++ queries.js
@@ -1,6 +1,26 @@
+/* 
+  Import modules/files you may need to correctly run the script. 
+  Make sure to save your DB's uri in the config file, then import it with a require statement!
+ */
+var fs = require('fs'),
+    mongoose = require('mongoose'), 
+    Schema = mongoose.Schema, 
+    Listing = require('./ListingSchema.js'), 
+    config = require('./config'),
+    mongo = require('mongodb').MongoClient,
+    assert = require('assert');
+
+    mongoose.Promise = global.Promise; 
+    mongoose.connect(config.db.uri, { useMongoClient: true });
+
 /* Fill out these functions using Mongoose queries*/
 
 var findLibraryWest = function() {
+  var query = { name: "Library West" };
+  Listing.find(query, function(err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
   /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
@@ -12,14 +32,36 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+
+   var query = { code: "CABL" };
+   Listing.findOne(query, function(err,result){
+    if (err) throw err;
+    console.log(result);
+   });
+   Listing.findOneAndRemove(query, function(err, result){
+    if (err) throw err;
+    //console.log(result);
+   });
 };
 var updatePhelpsLab = function() {
   /*
     Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+   var query = {name: "Phelps Laboratory"};
+   var upd = {address: "new Address"};
+   Listing.findOneAndUpdate(query, upd ,function(err, result){
+    if (err) throw err;
+    console.log(result);
+   });
 };
 var retrieveAllListings = function() {
+  var query = {};
+  Listing.find(query, function(err,result){
+    if (err) throw err;
+    console.log(result);
+    mongoose.disconnect();
+  });
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
@@ -29,3 +71,4 @@ findLibraryWest();
 removeCable();
 updatePhelpsLab();
 retrieveAllListings();
+
